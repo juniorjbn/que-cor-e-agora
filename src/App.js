09aa24ff4f1399b3eca3a9a5e5a47d0a696de85e
Joao <juniorjbn@gmail.com>
2018-10-16 15:45:14 -0300

    build new

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
           <div id="intro">
             <p className="introh3">
               <span className="dottedline_left"/>
-              Que cor é agora? #008
+              Que cor é agora? #009
               <span className="dottedline_right"/>
             </p>
             <p className="introh1" id="agora">{display}</p>
